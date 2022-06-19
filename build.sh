205380523235c0090cdec46b2637e16eb0a1a22e
Philip So <100310345+phil1sf@users.noreply.github.com>
2022-06-19 16:40:28 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#combining files into index html to directory docs
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-
-#combining files into about.html to directory docs
-cat templates/top.html content/about.html templates/bottom.html > docs/about.html
-
-#combining files into resume.html to directory docs
-cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
