642b5b2beccdb8614c811fee570079686121b559
Felipe S <flmwso@gmail.com>
2022-06-19 15:47:26 -0700

    new changes to bash script

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-#combining files into index html 
+#combining files into index html to directory docs
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+
+#combining files into about.html to directory docs
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+
+#combining files into resume.html to directory docs
+cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
