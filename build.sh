1907ee0d57928b79b1ea2a6a005b83678c80f1d5
Felipe S <flmwso@gmail.com>
2022-06-19 17:00:30 -0700

    new files created

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 # combining files into index.html
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+
+#combining files
+cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
+
+#combining again
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
