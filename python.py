de83b326b21ae7b8f1448e2b043b08cf6ddd18f9
Felipe S <flmwso@gmail.com>
2022-07-07 19:19:11 -0700

    saving

--- python.py
+++ python.py
@@ -1,38 +1,49 @@
+pages = [ 
+		{
+			"filename": "content/about.html",
+			"output": "docs/about.html",
+			"title": "About Me",
+		},
+		{
+			"filename": "content/index.html",
+			"output": "docs/index.html",
+			"title": "Main",
+		},
+		{
+			"filename": "content/resume.html",
+			"output": "docs/resume.html",
+			"title": "Resume",
+		}
+		]
+			
+			
+def main ():
+    for page in pages:
+        top_html = open('./templates/top.html').read()
+        bottom_html = open('./templates/bottom.html').read()
+        input_filecontents = page['filename']
+        content = open(input_filecontents).read()
+
+# Give this a try: "w+" can be used to overwrite a file if it exists or create a new file if it doesn't.
+        if input_filecontents == "./content/about.html":
+            about_html = top_html + content + bottom_html            
+            open('./docs/about.html', 'w+').write(about_html)
+        
+        if input_filecontents == "./content/index.html":
+            index_html = top_html + content + bottom_html
+            open('./docs/index.html', 'w+').write(index_html)
+        
+        if input_filecontents == "./content/resume.html":
+            resume_html = top_html + content + bottom_html
+            open('./docs/resume.html', 'w+').write(resume_html)
+            
+           
+for page in pages:
+    page_title = page['title']
+    main ()
+		
+	
 
 
-#reads in the top html
-top_html = open('./templates/top.html').read()
-
-#reads in the bottom.html
-bottom_html = open('./templates/bottom.html').read()
-
-#reads in the middle index.html
-middle_html = open('./content/index.html').read()
-
-#combine top middle and bottom into single html 
-combined_html = top_html + middle_html + bottom_html
-
-#writes new html to a brand new file in another directory
-open('./docs/index.html' , 'w+').write(combined_html)
 
 
-
-#reads in the middle index.html
-middle_html = open('./content/about.html').read()
-
-#combine top middle and bottom into single html 
-combined_html = top_html + middle_html + bottom_html
-
-#writes new html to a brand new file in same directory
-open('./docs/about.html' , 'w+').write(combined_html)
-
-
-
-#reads in the middle index.html
-middle_html = open('./content/resume.html').read()
-
-#combine top middle and bottom into single html 
-combined_html = top_html + middle_html + bottom_html
-
-#writes new html to a brand new file in same directory
-open('./docs/resume.html' , 'w+').write(combined_html)
